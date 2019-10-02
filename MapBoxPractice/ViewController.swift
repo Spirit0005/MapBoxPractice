ae7f134282e30c928a6fec018a354f30543d47a7
Spirit0005 <m_hamzak1997@yahoo.com>
2019-10-02 17:54:55 +0500

    Initial Commit

---
+++ MapBoxPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapBoxPractice
+//
+//  Created by 2 Minut on 10/2/19.
+//  Copyright © 2019 2 Minut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
